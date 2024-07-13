633ed883abe40b45d35163317d76bcf913ee4ff4
SoftGen <admin@softgen.ai>
2024-07-13 04:35:52 +0000

    SOFTGEN MAGIC

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,16 @@
 import "@/styles/globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { ThemeProvider } from "next-themes";
 import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <TooltipProvider>
-        <>
-          <Component {...pageProps} />
-          <Toaster />
-          <Analytics />
-        </>
-      </TooltipProvider>
-    </ThemeProvider>
+    <TooltipProvider>
+      <>
+        <Component {...pageProps} />
+        <Toaster />
+        <Analytics />
+      </>
+    </TooltipProvider>
   );
 }
